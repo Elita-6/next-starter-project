526fa32284e81db4f1bc99c497a46e253fd3a5f3
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-02 17:31:25 +0300

    setup project

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth"
+import {options} from "@/app/api/auth/[...nextauth]/options";
 
-export const authOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? ""
-        })
-    ],
+const handler = NextAuth(options)
 
-}
-
-export const handler = NextAuth(authOptions);
-export {handler as GET , handler as POST};
+export { handler as GET, handler as POST }
