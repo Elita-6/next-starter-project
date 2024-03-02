f376da469ee8e8e7366eb86a9fc195314bc1a759
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-02 17:47:10 +0300

    setup project

--- src/app/feat-exemple/_components/example_component.tsx
+++ src/app/feat-exemple/_components/example_component.tsx
@@ -1,9 +1,16 @@
+'use client'
 import React from 'react';
+import {useSession} from "next-auth/react";
 
 const ExampleComponent = () => {
+    const session = useSession()
     return (
         <div>
-
+            {
+                session.data && (
+                    <p>{session.data.user?.name}</p>
+                )
+            }
         </div>
     );
 };
