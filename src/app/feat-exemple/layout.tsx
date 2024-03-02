015c232d50e2d471ff55987c05222130d67dfe67
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-02 16:02:16 +0300

    setup project

---
+++ src/app/feat-exemple/layout.tsx
@@ -0,0 +1,11 @@
+import React, {PropsWithChildren} from 'react';
+
+const ExampleLayout = ({ children }: PropsWithChildren) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default ExampleLayout;
