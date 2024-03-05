3479b3b257e913daefea47d49c521afc7753c1bc
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-03-05 13:26:27 +0300

    add base component from shadcn

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -14,7 +14,7 @@ export default function SignIn(){
         <div className='flex flex-col items-center space-y-8 '>
             <h2 className="text-4xl font-bold">.Logo</h2>
             <div className='flex flex-col space-y-3 items-center'>
-                <h3 className="text-2xl font-semibold">Let's get started</h3>
+                <h3 className="text-2xl font-semibold">Let`&apos`s get started</h3>
                 <p className='font-light'>Sign up to the family and get stared immediately</p>
             </div>
             <div className='flex flex-col space-y-2 w-[25%]'>
